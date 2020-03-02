6ed8831f8ab3d8459a50076bd5e013adaf34f3c8
George <admin@georgesimos.com>
2020-03-02 12:12:07 +0200

    feat: handle authenticated state

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,18 +1,35 @@
 import React from "react";
 import { Link } from "gatsby";
+import { useIdentityContext } from "react-netlify-identity";
 
-const Profile = () => (
-  <div className="dashboard-header">
-    <nav>
-      <Link to="dashboard/secret" activeClassName="active">
-        Secret Stuff
-      </Link>
-      <Link to="dashboard/base" activeClassName="active">
-        See Your Base
-      </Link>
-    </nav>
-    <span>TODO: Show login status</span>
-  </div>
-);
+const Profile = ({ showModal }) => {
+  const identify = useIdentityContext();
+  const isLoggedIn = identify && identify.isLoggedIn;
+
+  const name =
+    identify &&
+    identify.user &&
+    identify.user.user_metadata &&
+    identify.user.user_metadata.full_name;
+
+  return (
+    isLoggedIn && (
+      <div className="dashboard-header">
+        <nav>
+          <Link to="dashboard/secret" activeClassName="active">
+            Secret Stuff
+          </Link>
+          <Link to="dashboard/base" activeClassName="active">
+            See Your Base
+          </Link>
+        </nav>
+        <span>
+          Logged in as {name}
+          <button onClick={showModal}>Log out</button>
+        </span>
+      </div>
+    )
+  );
+};
 
 export default Profile;
--- src/components/route-login.js
+++ src/components/route-login.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { useIdentityContext } from "react-netlify-identity-widget";
+import { navigate } from "gatsby";
 
-const RouteLogin = () => (
-  <>
-    <h1>Log In or Sign Up</h1>
-    <button>Login</button>
-  </>
-);
+const RouteLogin = ({ showModal }) => {
+  const identity = useIdentityContext();
+  console.log(identity);
+
+  if (identity && identity.isLoggedIn) {
+    navigate("/dashboard/secret", { replace: true });
+  }
+  return (
+    <>
+      <h1>Log In or Sign Up</h1>
+      <button onClick={showModal}>Login</button>
+    </>
+  );
+};
 
 export default RouteLogin;
